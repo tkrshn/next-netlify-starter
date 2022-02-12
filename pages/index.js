532200f1ac09690a23570f612d29dd665e911038
Thulasi Krishnan <tkrishnan@hey.com>
2022-02-12 14:35:20 -0500

    added a custom favicon

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,11 @@ export default function Home() {
     <div className="container">
       <Head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />   
-        <title>About - tkrshn</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>tkrshn</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
       </Head>
       <main>
         <Header title="👋 Hey there!" />
